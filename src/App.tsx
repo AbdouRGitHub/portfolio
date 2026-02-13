8f36dd940d92af5d45966018e1820905b23c7ad0
Abdou <abdousani60@gmail.com>
2026-02-13 16:41:04 +0100

    feat: projet initial

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import './App.css';
+
+
+function App() {
+
+    return (
+        <>
+            <main className="app">
+                <div className="profileSection">
+                    <div className="photoContainer">
+                        <img src="/photo.jpg" alt="photo" className="photo"/>
+                    </div>
+                    <h1 className="name">Abdou-Rahmane</h1>
+                    <p className="description">
+                        Développeur fullstack avec 2 ans d’expérience en backend dans une ad tech spécialisé dans le
+                        domaine de la santé,
+                        je conçois des applications web utilisant des technologies comme NestJS, Spring et React.
+                    </p>
+                    <nav>
+                        <ul>
+                            <li>
+
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </main>
+            <footer></footer>
+        </>
+    )
+}
+
+export default App
